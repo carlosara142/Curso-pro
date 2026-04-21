e82e363385e15064983baf23f857db686403b525
Carlosara142 <carlosara142@gmail.com>
2026-04-20 22:49:59 -0300

    con calculadora match

--- calculadoramatsh.py
+++ calculadoramatsh.py
@@ -14,22 +14,23 @@ while True:
     a = float(input("Ingrese el primer número: "))
     b = float(input("Ingrese el segundo número: "))
 
-    if opcion == '1':
-        resultado = a + b
-        print(f"El resultado de {a} + {b} es: {resultado}")
-    elif opcion == '2':
-        resultado = a - b
-        print(f"El resultado de {a} - {b} es: {resultado}")
-    elif opcion == '3':
-        resultado = a * b
-        print(f"El resultado de {a} * {b} es: {resultado}")
-    elif opcion == '4':
-        if b != 0:
-            resultado = a / b
-            print(f"El resultado de {a} / {b} es: {resultado}")
-        else:
-            print("Error: No se puede dividir por cero.")   
+    match opcion:
+        case '1':
+            resultado = a + b
+            print(f"El resultado de {a} + {b} es: {resultado}")
+        case '2':
+            resultado = a - b
+            print(f"El resultado de {a} - {b} es: {resultado}")
+        case '3':
+            resultado = a * b
+            print(f"El resultado de {a} * {b} es: {resultado}") 
+        case '4':
+            if b != 0:
+                resultado = a / b
+                print(f"El resultado de {a} / {b} es: {resultado}")    
+            else:
+                print("Error: No se puede dividir por cero.")   
     otra = input("¿Desea realizar otra operación? (s/n): ")
-    if otra != 's':
+    if otra != 's': 
         print("Gracias por usar la calculadora. ¡Hasta luego!")
         break
